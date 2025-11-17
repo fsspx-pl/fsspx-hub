90a6071210c8433302573ddf1cf60c5eb309520f
Jacek Adamczyk <jacek@adamczyk.me>
2025-11-17 23:41:41 +0100

    only inject posthog for setup env vars

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,14 @@
 import posthog from "posthog-js"
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://eu.posthog.com",
-  defaults: '2025-05-24',
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking
-  debug: process.env.NODE_ENV === "development",
-})
+if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: "/ingest",
+    ui_host: "https://eu.posthog.com",
+    defaults: '2025-05-24',
+    capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+    debug: process.env.NODE_ENV === "development",
+  });
+} else {
+  console.warn('NEXT_PUBLIC_POSTHOG_KEY is not set, skipping Posthog initialization');
+}
+            
